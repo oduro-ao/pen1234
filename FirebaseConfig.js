24b09c2f006160ee9be4fede32a4ddfe7f4daa7a
oduro-ao <oadu-owusu@st.ug.edu.gh>
2024-07-03 00:50:22 +0000

    final commit

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCz_YS3KEbxZhRHfUTPPGh23z4KcGVvoVM",
-  authDomain: "test-29693.firebaseapp.com",
-  projectId: "test-29693",
-  storageBucket: "test-29693.appspot.com",
-  messagingSenderId: "566302227632",
-  appId: "1:566302227632:web:252d9fe4dabf42780a8f75"
+  apiKey: "AIzaSyDpIkzWoGwh67r3ewzgTL5UqZw_zZtgrJQ",
+  authDomain: "peniel-bd099.firebaseapp.com",
+  projectId: "peniel-bd099",
+  storageBucket: "peniel-bd099.appspot.com",
+  messagingSenderId: "1077687082209",
+  appId: "1:1077687082209:web:266f1239531b757bca6ba0"
 };
 
 // Initialize Firebase
